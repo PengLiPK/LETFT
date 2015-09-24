d681bdea891d2890bf35fd39507f99a273349b7a
PengLiPK <00p000atgm@gmail.com>
2015-09-23 23:13:11 -0400

    Modified codes

---
+++ script_syn-test_single-phase/cp_runsyn_tomo.bash
@@ -0,0 +1,273 @@
+#!/bin/bash
+
+# Parameters of working dir
+#######################################################
+wkdir=syn_work
+inpfdir=input_file
+#######################################################
+
+# Parameters of input file
+######################################################
+initvel_file=synvel_init.txt
+data_file=syndata_init.txt
+#######################################################
+
+# Parameters of topography file
+#######################################################
+topo_file=hawaii_tp4tomo2.xyz
+topo_minlon=203.80
+topo_maxlon=205.383
+topo_minlan=18.80
+topo_maxlan=20.383
+topo_xnum=96
+topo_ynum=96
+topo_depth=9.673
+topo_vair=2.0
+#######################################################
+
+# Parameters of tomography
+#######################################################
+damp=100
+nevn=2493
+nsl=3264
+ndata=25465
+sta_num=35
+minlon=204.50
+maxlon=205.20
+minlan=19.20
+maxlan=19.70
+minz=0
+maxz=25.0
+fmm_dx=0.005
+fmm_dy=0.005
+fmm_dz=0.5
+fmm_rf_nx=5.0
+fmm_rf_ny=5.0
+fmm_rf_nz=5.0
+fmm_rf_x=0.045
+fmm_rf_y=0.045
+fmm_rf_z=5
+#######################################################
+
+
+# Check working dir is exist or not.
+if [ ! -d $wkdir ]
+then
+	echo "There is no dir called $wkdir!"
+	echo "You need to run cp_runsyn_vel_t.bash first!"
+	exit
+fi
+if [ ! -d $wkdir/syn_tomo/ ]
+then
+	mkdir $wkdir/syn_tomo/
+fi
+		
+# Copy input files
+cp ./$inpfdir/$topo_file ./$wkdir/syn_tomo/
+cp ./$wkdir/syn_outf/$initvel_file ./$wkdir/syn_tomo/
+cp ./$wkdir/syn_outf/$data_file ./$wkdir/syn_tomo/
+
+
+# Copy files to tomography directory
+cp ./src/fmm_fw_regul2tprl ./$wkdir/syn_tomo/
+cp ./src/fd2csm_row ./$wkdir/syn_tomo/
+cp ./src/fdsep_qr ./$wkdir/syn_tomo/
+cp ./src/fd2csm ./$wkdir/syn_tomo/
+cp ./src/calmdres2 ./$wkdir/syn_tomo/
+cp ./src/invsvddt_damp ./$wkdir/syn_tomo/
+cp ./src/invctrlds ./$wkdir/syn_tomo/
+cp ./src/invloc_svd_wei ./$wkdir/syn_tomo/
+cp ./src/dloc2loc_multi ./$wkdir/syn_tomo/
+
+
+# Run tomography program
+# Enter work dir
+cd ./$wkdir/syn_tomo/
+pwd
+mv $data_file syndataloc.txt
+mv $initvel_file vel.txt
+
+cp syndataloc.txt syndataloc_0.txt
+for((i=1;i<=2;i++))
+do
+
+# Forward
+	echo "syndataloc.txt" > fmm_fw_regul2tprl.inp
+	echo "vel.txt" >> fmm_fw_regul2tprl.inp
+	echo "$sta_num" >> fmm_fw_regul2tprl.inp
+	echo "$fmm_dx $fmm_dy $fmm_dz" >> fmm_fw_regul2tprl.inp
+	echo "$fmm_rf_nx $fmm_rf_ny $fmm_rf_nz" >> fmm_fw_regul2tprl.inp
+	echo "$fmm_rf_x $fmm_rf_y $fmm_rf_z" >> fmm_fw_regul2tprl.inp
+	echo "$minlon $maxlon" >> fmm_fw_regul2tprl.inp
+	echo "$minlan $maxlan" >> fmm_fw_regul2tprl.inp
+	echo "$minz $maxz" >> fmm_fw_regul2tprl.inp
+	echo "2" >> fmm_fw_regul2tprl.inp
+	echo "2" >> fmm_fw_regul2tprl.inp
+	echo "$topo_file" >> fmm_fw_regul2tprl.inp
+	echo "$topo_minlon $topo_maxlon" >> fmm_fw_regul2tprl.inp
+	echo "$topo_minlan $topo_maxlan" >> fmm_fw_regul2tprl.inp
+	echo "$topo_xnum $topo_ynum" >> fmm_fw_regul2tprl.inp
+	echo "$topo_depth" >> fmm_fw_regul2tprl.inp
+	echo "$topo_vair" >> fmm_fw_regul2tprl.inp
+	./fmm_fw_regul2tprl
+	echo "Forward finished!"
+
+# Separate slowness derivative matrix form fd
+
+	awk '{if(NF==9)print $1,$9}' syndataloc.txt > tempt
+	paste t.txt tempt | awk '{print ($1+$6)-($5+0.1)}' > dt.txt
+	j=$(( $i - 1 ))
+	cp dt.txt res_$j.txt
+
+	echo "fd.dat" > fd2csm_row.inp
+	echo "metafd_row.dat" >> fd2csm_row.inp
+	echo "fdcsm_row.dat" >> fd2csm_row.inp
+	echo "$ndata" >> fd2csm_row.inp
+	./fd2csm_row
+
+	
+	echo "metafd_row.dat" > fdsep_qr.inp
+	echo "fdcsm_row.dat" >> fdsep_qr.inp
+	echo "dt.txt" >> fdsep_qr.inp
+	echo "fdloc.dat" >> fdsep_qr.inp
+	echo "dtloc.txt" >> fdsep_qr.inp
+	echo "rloc.dat" >> fdsep_qr.inp
+	echo "fdsep.dat" >> fdsep_qr.inp
+	echo "dtsep.txt" >> fdsep_qr.inp
+	echo "$ndata $nevn $nsl" >> fdsep_qr.inp
+	./fdsep_qr
+   
+
+# Prepare input for inverse
+
+	echo "fdsep.dat" > fd2csm.inp
+	echo "metafd.dat" >> fd2csm.inp
+	echo "fdcsm.dat" >> fd2csm.inp
+	echo "$nsl" >> fd2csm.inp
+	./fd2csm
+
+
+	nsldata=$(wc -l dtsep.txt | awk '{print $1}')
+
+# Run calmdres2, calculating DWS values, ray counts
+    echo "metafd.dat" > calmdres2.inp
+    echo "fdcsm.dat" >> calmdres2.inp
+    echo "rct.txt" >> calmdres2.inp
+    echo "dws.txt" >> calmdres2.inp
+    echo "$nsldata $nsl" >> calmdres2.inp
+  
+    ./calmdres2
+    echo "Run calmdres2 finished!!"
+
+	echo " Prepare input for inverse finished!"
+
+
+# Run invsvddt_damp
+    echo "dtsep.txt" > invsvddt_damp.inp
+    echo "metafd.dat" >> invsvddt_damp.inp
+	echo "fdcsm.dat" >> invsvddt_damp.inp
+    echo "out_dsl.txt" >> invsvddt_damp.inp
+    echo "$nsldata $nsl" >> invsvddt_damp.inp
+    echo "$damp" >> invsvddt_damp.inp
+
+    ./invsvddt_damp
+
+
+	echo "Inversion part finished!!"
+	
+	cp out_dsl.txt out_dsl$i.txt
+	cp vel.txt vel_$i.txt
+
+# Convert results of invsvddt to real velocity
+    echo "vel.txt" > invctrlds.inp
+	echo "out_dsl.txt" >> invctrlds.inp
+	echo "vel_$i.txt" >> invctrlds.inp
+	echo "dws.txt" >> invctrlds.inp
+	echo "50" >> invctrlds.inp
+    echo "0.105 1" >> invctrlds.inp
+    echo "-0.06 0.06" >> invctrlds.inp
+
+    ./invctrlds
+
+
+
+# Relocation
+
+for((k=1;k<=1;k++))
+do
+# Forward
+	echo "syndataloc.txt" > fmm_fw_regul2tprl.inp
+	echo "vel.txt" >> fmm_fw_regul2tprl.inp
+	echo "$sta_num" >> fmm_fw_regul2tprl.inp
+	echo "$fmm_dx $fmm_dy $fmm_dz" >> fmm_fw_regul2tprl.inp
+	echo "$fmm_rf_nx $fmm_rf_ny $fmm_rf_nz" >> fmm_fw_regul2tprl.inp
+	echo "$fmm_rf_x $fmm_rf_y $fmm_rf_z" >> fmm_fw_regul2tprl.inp
+	echo "$minlon $maxlon" >> fmm_fw_regul2tprl.inp
+	echo "$minlan $maxlan" >> fmm_fw_regul2tprl.inp
+	echo "$minz $maxz" >> fmm_fw_regul2tprl.inp
+	echo "2" >> fmm_fw_regul2tprl.inp
+	echo "2" >> fmm_fw_regul2tprl.inp
+	echo "$topo_file" >> fmm_fw_regul2tprl.inp
+	echo "$topo_minlon $topo_maxlon" >> fmm_fw_regul2tprl.inp
+	echo "$topo_minlan $topo_maxlan" >> fmm_fw_regul2tprl.inp
+	echo "$topo_xnum $topo_ynum" >> fmm_fw_regul2tprl.inp
+	echo "$topo_depth" >> fmm_fw_regul2tprl.inp
+	echo "$topo_vair" >> fmm_fw_regul2tprl.inp
+	./fmm_fw_regul2tprl
+	echo "Forward finished!"
+
+# Prepare input for inverse
+	echo "fd.dat" > fd2csm_row.inp
+	echo "metafd_row.dat" >> fd2csm_row.inp
+	echo "fdcsm_row.dat" >> fd2csm_row.inp
+	echo "$ndata" >> fd2csm_row.inp
+	./fd2csm_row
+
+	awk '{if(NF==9)print $1,$9}' syndataloc.txt > temporigt
+	paste t.txt temporigt | awk '{print ($1+$6)-($5+0.1)}' > dt2.txt
+
+	echo " Prepare input for inverse finished!"
+
+
+# Run invloc_svd_wei
+    echo "metafd_row.dat" > invloc_svd_wei.inp
+    echo "fdcsm_row.dat" >> invloc_svd_wei.inp
+    echo "dt2.txt" >> invloc_svd_wei.inp
+    echo "out_loc.txt" >> invloc_svd_wei.inp
+    echo "$ndata $nevn $nsl" >> invloc_svd_wei.inp
+    echo "100000" >> invloc_svd_wei.inp
+
+    ./invloc_svd_wei
+    echo "Inversion part finished!!"
+
+	cp syndataloc.txt syndataloctmp_$k.txt
+
+# Convert results of invsvddt to real locations
+	echo "syndataloctmp_$k.txt" > dloc2loc_multi.inp
+	echo "out_loc.txt" >> dloc2loc_multi.inp
+	echo "syndataloc.txt" >> dloc2loc_multi.inp
+	echo "$sta_num $nevn" >> dloc2loc_multi.inp
+	echo "0.25 0.08" >> dloc2loc_multi.inp
+	echo "$minlon $maxlon" >> dloc2loc_multi.inp
+	echo "$minlan $maxlan" >> dloc2loc_multi.inp
+	echo "$minz $maxz" >> dloc2loc_multi.inp
+	echo "$topo_file" >> dloc2loc_multi.inp
+	echo "$topo_minlon $topo_maxlon" >> dloc2loc_multi.inp
+	echo "$topo_minlan $topo_maxlan" >> dloc2loc_multi.inp
+	echo "$topo_xnum $topo_ynum" >> dloc2loc_multi.inp
+
+	./dloc2loc_multi
+done
+    cp syndataloc.txt syndataloc_$i.txt
+	cp out_loc.txt out_loc$i.txt
+
+done
+
+rm paths.dat errt.txt
+
+cp vel.txt ../syn_outf/vel_final.txt
+cp syndataloc.txt ../syn_outf/syndata_out.txt
+
+# exit work dir
+cd ../../
+pwd
---
+++ script_syn-test_single-phase/cp_runsyn_vel_t.bash
@@ -0,0 +1,156 @@
+#!/bin/bash
+
+# Parameters of working dir
+#######################################################
+wkdir=syn_work
+inpfdir=input_file
+#######################################################
+
+# Parameters of topography file
+#######################################################
+topo_file=hawaii_tp4tomo2.xyz
+topo_minlon=203.80
+topo_maxlon=205.383
+topo_minlan=18.80
+topo_maxlan=20.383
+topo_xnum=96
+topo_ynum=96
+topo_depth=9.673
+topo_vair=2.0
+#######################################################
+
+# Parameters of synthetic velocity model construction
+#######################################################
+node_structure=vstrct_tp3km.txt
+oned_vel=vz.1D_inverted
+vel_pertubation=0.1 # 0.1 is 10% from 1D velocity model.
+#######################################################
+
+# Parameters of synthetic travel times construction
+#######################################################
+data_file=p_data.txt
+sta_num=35
+minlon=204.50
+maxlon=205.20
+minlan=19.20
+maxlan=19.70
+minz=0
+maxz=25.0
+fmm_dx=0.005
+fmm_dy=0.005
+fmm_dz=0.5
+fmm_rf_nx=5.0
+fmm_rf_ny=5.0
+fmm_rf_nz=5.0
+fmm_rf_x=0.045
+fmm_rf_y=0.045
+fmm_rf_z=5
+#######################################################
+
+# Paramters of constructing input data for synthetic tomograpy.
+#######################################################
+gs_mean=0
+gs_std=0.5
+loc_amp=1
+ot_amp=0.1
+#######################################################
+
+
+
+# Check working dir is exist or not.
+if [ ! -d $wkdir ]
+then
+	mkdir $wkdir
+fi
+
+if [ ! -d $wkdir/syn_vel_t/ ]
+then
+	mkdir $wkdir/syn_vel_t/
+fi
+
+if [ ! -d $wkdir/syn_outf/ ]
+then
+	mkdir $wkdir/syn_outf/
+fi
+
+# Enter $wkdir/syn_vel_t
+cd $wkdir/syn_vel_t
+
+# Copy input files
+cp ../../$inpfdir/$node_structure .
+cp ../../$inpfdir/$topo_file .
+cp ../../$inpfdir/$oned_vel .
+cp ../../$inpfdir/$data_file .
+
+cp ../../src/prevel3d .
+cp ../../src/fmm_synt3d_v .
+cp ../../src/genGSnoise .
+cp ../../src/migraloc .
+
+# Generate synthetic velocity model from 1D velocity model.
+echo "$node_structure" > prevel3d.inp
+echo "syndvel.txt" >> prevel3d.inp
+echo "synvel.txt" >> prevel3d.inp
+echo "synvel_init.txt" >> prevel3d.inp
+echo "$vel_pertubation" >> prevel3d.inp
+echo "$oned_vel" >> prevel3d.inp
+echo "$topo_file" >> prevel3d.inp
+echo "$topo_minlon $topo_maxlon" >> prevel3d.inp
+echo "$topo_minlan $topo_maxlan" >> prevel3d.inp
+echo "$topo_xnum $topo_ynum" >> prevel3d.inp
+echo "$topo_depth" >> prevel3d.inp
+echo "$topo_vair" >> prevel3d.inp
+./prevel3d
+
+# Generate synthetic travel time data from synthetic velocity model.
+echo "$data_file" > fmm_synt3d_v.inp
+echo "synvel.txt" >> fmm_synt3d_v.inp
+echo "$sta_num" >> fmm_synt3d_v.inp
+echo "$fmm_dx $fmm_dy $fmm_dz" >> fmm_synt3d_v.inp
+echo "$fmm_rf_nx $fmm_rf_ny $fmm_rf_nz" >> fmm_synt3d_v.inp
+echo "$fmm_rf_x $fmm_rf_y $fmm_rf_z" >> fmm_synt3d_v.inp
+echo "$minlon $maxlon" >> fmm_synt3d_v.inp
+echo "$minlan $maxlan" >> fmm_synt3d_v.inp
+echo "$minz $maxz" >> fmm_synt3d_v.inp
+echo "2" >> fmm_synt3d_v.inp
+echo "2" >> fmm_synt3d_v.inp
+echo "$topo_file" >> fmm_synt3d_v.inp
+echo "$topo_minlon $topo_maxlon" >> fmm_synt3d_v.inp
+echo "$topo_minlan $topo_maxlan" >> fmm_synt3d_v.inp
+echo "$topo_xnum $topo_ynum" >> fmm_synt3d_v.inp
+echo "$topo_depth" >> fmm_synt3d_v.inp
+echo "$topo_vair" >> fmm_synt3d_v.inp
+./fmm_synt3d_v
+mv t.txt syndata.txt
+
+# Add Gaussian noise to the locations and origin times of synthetic data.
+# The output files will be used as input data for synthetic tomography.
+echo "2" > genGSnoise.inp
+echo "syndata.txt" >> genGSnoise.inp
+echo "syndata_init_temp.txt" >> genGSnoise.inp
+echo "$sta_num" >> genGSnoise.inp
+echo "$minlon $maxlon" >> genGSnoise.inp
+echo "$minlan $maxlan" >> genGSnoise.inp
+echo "$minz $maxz" >> genGSnoise.inp
+echo "$gs_mean $gs_std $loc_amp $ot_amp" >> genGSnoise.inp
+echo "2" >> genGSnoise.inp
+echo "$topo_file" >> genGSnoise.inp
+echo "$topo_minlon $topo_maxlon" >> genGSnoise.inp
+echo "$topo_minlan $topo_maxlan" >> genGSnoise.inp
+echo "$topo_xnum $topo_ynum" >> genGSnoise.inp
+./genGSnoise
+
+# Migrate the locations above surface below the surface.
+echo "syndata_init_temp.txt" >> migraloc.inp
+echo "syndata_init.txt" >> migraloc.inp
+echo "$sta_num" >> migraloc.inp
+echo "$topo_file" >> migraloc.inp
+echo "$topo_minlon $topo_maxlon" >> migraloc.inp
+echo "$topo_minlan $topo_maxlan" >> migraloc.inp
+echo "$topo_xnum $topo_ynum" >> migraloc.inp
+./migraloc
+
+cp syndata.txt syndata_init.txt synvel.txt synvel_init.txt ../syn_outf
+
+# Exit working dir
+cd ../../
