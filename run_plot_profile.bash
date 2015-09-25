ea1d7889259f0cdeab0e67ee9b2611c1079470b0
PengLiPK <00p000atgm@gmail.com>
2015-09-25 15:28:33 -0400

    Modified codes

--- run_plot_profile.bash
+++ run_plot_profile.bash
@@ -2,7 +2,7 @@
 
 # Parameters of working dir
 #######################################################
-wkdir=work
+wkdir=work_p
 inpfdir=outf
 inpfdir_1dv=input_file
 plotdir=plot
@@ -10,11 +10,8 @@ plotdir=plot
 
 # Parameters of input files
 #######################################################
-reff=synvel_init.txt
-inpf1=synvel.txt
-inpf2=vel_final.txt
+inpf1=vel_final.txt
 modresf=mdres.txt
-onedvfile=vz.1D_inverted
 #######################################################
 
 # Parameters of topography file
@@ -27,21 +24,20 @@ topo_maxlan=20.383
 topo_xnum=96
 topo_ynum=96
 topo_depth=9.673
-topo_vair=2.0
+topo_vair=2.5
 #######################################################
 
 # Parameters of plotting
 #######################################################
 thrshd=0.4 # Model resolution > 0.4 will be plotted.
-minlon=204.50
-maxlon=205.20
-minlan=19.38
-maxlan=19.38
 minz=0
 maxz=25.0
-dx=0.005
-dy=0.005
-dz=0.5
+dx=0.5
+dy=0.5
+lineseg=3
+x[1]=204.66; y[1]=19.46
+x[2]=204.8; y[2]=19.35
+x[3]=205.2; y[3]=19.52
 #######################################################
 
 # Check working dir is exist or not.
@@ -63,18 +59,13 @@ then
 fi
 
 # Copy files
-cp ./$wkdir/$inpfdir/$reff ./$wkdir/$plotdir/
 cp ./$wkdir/$inpfdir/$inpf1 ./$wkdir/$plotdir/
-cp ./$wkdir/$inpfdir/$inpf2 ./$wkdir/$plotdir/
 cp ./$wkdir/$inpfdir/$modresf ./$wkdir/$plotdir/
-cp ./$inpfdir_1dv/$onedvfile ./$wkdir/$plotdir/
 cp ./$inpfdir_1dv/$topo_file ./$wkdir/$plotdir/
 
-cp ./src_plot/cutvdiff_reg1_xyz ./$wkdir/$plotdir/
-cp ./src_plot/cutvdiff_reg2_xyz ./$wkdir/$plotdir/
-cp ./src_plot/cut_ressl_reg2_xyz ./$wkdir/$plotdir/
+cp ./src_plot/cutvdiff_line2_xyz ./$wkdir/$plotdir/
 cp ./src_plot/valmask ./$wkdir/$plotdir/
-cp ./src_plot/plot_v_xyz.bash ./$wkdir/$plotdir/
+cp ./src_plot/plot_v_xyz_line.bash ./$wkdir/$plotdir/
 
 
 # Enter working dir
@@ -82,92 +73,33 @@ cd ./$wkdir/$plotdir/
 pwd
 
 # Files for plotting
-num=$(head -1 $reff | awk '{print $1}')
-head -2 $inpf2 > truevelper.txt
-paste $inpf1 $reff | tail -$num | awk '{print $1,$2,$3,($4-$8)/$8}' >> truevelper.txt
-
-head -2 $inpf2 > velper_tp.txt
-paste $inpf2 $reff | tail -$num | awk '{print $1,$2,$3,($4-$8)/$8}' >> velper_tp.txt
-
-head -2 $inpf2 > velperdiff_tp.txt
-paste $inpf2 $inpf1 $reff | tail -$num | awk '{print $1,$2,$3,($4-$8)/$12}' >> velperdiff_tp.txt
-
+echo "$lineseg" > tmpsegs.txt
+for((i=1;i<=$lineseg;i++))
+do
+	echo "${x[$i]} ${y[$i]}" >> tmpsegs.txt
+done
 
 # cut slice of truevel
-echo "slice_truevel.txt" > cutvdiff_reg1_xyz.inp
-echo "truevelper.txt" >> cutvdiff_reg1_xyz.inp
-echo "$minlon $maxlon" >> cutvdiff_reg1_xyz.inp
-echo "$minlan $maxlan" >> cutvdiff_reg1_xyz.inp
-echo "$minz $maxz" >> cutvdiff_reg1_xyz.inp
-echo "$dx $dy $dz" >> cutvdiff_reg1_xyz.inp
-echo "$topo_file" >> cutvdiff_reg1_xyz.inp
-echo "$topo_minlon $topo_maxlon" >> cutvdiff_reg1_xyz.inp
-echo "$topo_minlan $topo_maxlan" >> cutvdiff_reg1_xyz.inp
-echo "$topo_xnum $topo_ynum" >> cutvdiff_reg1_xyz.inp
-echo "$topo_depth" >> cutvdiff_reg1_xyz.inp
-echo "$topo_vair" >> cutvdiff_reg1_xyz.inp
-
-./cutvdiff_reg1_xyz
-
-
-# cut slice of vel_tp
-echo "slice_veltp.txt" > cutvdiff_reg1_xyz.inp
-echo "velper_tp.txt" >> cutvdiff_reg1_xyz.inp
-echo "$minlon $maxlon" >> cutvdiff_reg1_xyz.inp
-echo "$minlan $maxlan" >> cutvdiff_reg1_xyz.inp
-echo "$minz $maxz" >> cutvdiff_reg1_xyz.inp
-echo "$dx $dy $dz" >> cutvdiff_reg1_xyz.inp
-echo "$topo_file" >> cutvdiff_reg1_xyz.inp
-echo "$topo_minlon $topo_maxlon" >> cutvdiff_reg1_xyz.inp
-echo "$topo_minlan $topo_maxlan" >> cutvdiff_reg1_xyz.inp
-echo "$topo_xnum $topo_ynum" >> cutvdiff_reg1_xyz.inp
-echo "$topo_depth" >> cutvdiff_reg1_xyz.inp
-echo "$topo_vair" >> cutvdiff_reg1_xyz.inp
-
-./cutvdiff_reg1_xyz
-
-
-# cut slice of velperdiff_tp
-echo "slice_veldifftp.txt" > cutvdiff_reg2_xyz.inp
-echo "velperdiff_tp.txt" >> cutvdiff_reg2_xyz.inp
-echo "$modresf $thrshd" >> cutvdiff_reg2_xyz.inp
-echo "$minlon $maxlon" >> cutvdiff_reg2_xyz.inp
-echo "$minlan $maxlan" >> cutvdiff_reg2_xyz.inp
-echo "$minz $maxz" >> cutvdiff_reg2_xyz.inp
-echo "$dx $dy $dz" >> cutvdiff_reg2_xyz.inp
-echo "$topo_file" >> cutvdiff_reg2_xyz.inp
-echo "$topo_minlon $topo_maxlon" >> cutvdiff_reg2_xyz.inp
-echo "$topo_minlan $topo_maxlan" >> cutvdiff_reg2_xyz.inp
-echo "$topo_xnum $topo_ynum" >> cutvdiff_reg2_xyz.inp
-echo "$topo_depth" >> cutvdiff_reg2_xyz.inp
-echo "$topo_vair" >> cutvdiff_reg2_xyz.inp
-
-./cutvdiff_reg2_xyz
-
-# cut model resolution slice
-echo "slice_mdres.txt" > cut_ressl_reg2_xyz.inp
-echo "$modresf" >> cut_ressl_reg2_xyz.inp
-echo "$reff" >> cut_ressl_reg2_xyz.inp
-echo "$minlon $maxlon" >> cut_ressl_reg2_xyz.inp
-echo "$minlan $maxlan" >> cut_ressl_reg2_xyz.inp
-echo "$minz $maxz" >> cut_ressl_reg2_xyz.inp
-echo "$dx $dy $dz" >> cut_ressl_reg2_xyz.inp
-echo "$topo_file" >> cut_ressl_reg2_xyz.inp
-echo "$topo_minlon $topo_maxlon" >> cut_ressl_reg2_xyz.inp
-echo "$topo_minlan $topo_maxlan" >> cut_ressl_reg2_xyz.inp
-echo "$topo_xnum $topo_ynum" >> cut_ressl_reg2_xyz.inp
-echo "$topo_depth" >> cut_ressl_reg2_xyz.inp
-echo "$topo_vair" >> cut_ressl_reg2_xyz.inp
-
-./cut_ressl_reg2_xyz
-
-echo "0.00000 0.00000 0.000000" > elev_poly.txt
-cat elev_line.txt >> elev_poly.txt
-tail -1 elev_line.txt | awk '{print $1,0.000,0.000}' >> elev_poly.txt
-echo "0.00000 0.00000 0.000000" >> elev_poly.txt
+echo "slice_veltp_1.txt" > cutvdiff_line2_xyz.inp
+echo "slice_mdres_1.txt" >> cutvdiff_line2_xyz.inp
+echo "ndprj_1.txt 1.5" >> cutvdiff_line2_xyz.inp
+echo "$inpf1" >> cutvdiff_line2_xyz.inp
+echo "$modresf $thrshd" >> cutvdiff_line2_xyz.inp
+echo "tmpsegs.txt" >> cutvdiff_line2_xyz.inp
+echo "$minz $maxz" >> cutvdiff_line2_xyz.inp
+echo "$dx $dy" >> cutvdiff_line2_xyz.inp
+echo "$topo_file" >> cutvdiff_line2_xyz.inp
+echo "$topo_minlon $topo_maxlon" >> cutvdiff_line2_xyz.inp
+echo "$topo_minlan $topo_maxlan" >> cutvdiff_line2_xyz.inp
+echo "$topo_xnum $topo_ynum" >> cutvdiff_line2_xyz.inp
+echo "$topo_depth" >> cutvdiff_line2_xyz.inp
+echo "$topo_vair" >> cutvdiff_line2_xyz.inp
+./cutvdiff_line2_xyz
+mv elev_line.txt elev_line1.txt
+mv elev_poly.txt elev_poly1.txt
 
 # Plot velocity and pertubation of velocity
-bash plot_v_xyz.bash $thrshd
+bash plot_v_xyz_line.bash $thrshd
 cp *.ps *.eps ../$inpfdir
 
 # Exit dir
